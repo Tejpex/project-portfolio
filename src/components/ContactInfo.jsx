96cd05b5ecb19cfed09dfa80634561213a3e9e23
Tejpex <equis_angel@hotmail.com>
2024-03-28 12:08:45 +0100

    Worked  with styling of contact info

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,9 +1,15 @@
 import "./ContactInfo.css"
-import profilePicture from "../assets/CV-bild-vanster.jpg";
+import profilePicture from "../assets/CV-bild-vanster.jpg"
+import linkedIn from "../assets/linkedin.svg"
+import githubSmall from "../assets/github-small.svg"
+import stackOverflow from "../assets/stackoverflow.svg"
+import instagram from "../assets/instagram.svg"
+import blueSky from "../assets/bluesky.png"
+
 
 export const ContactInfo = () => {
   return (
-    <div>
+    <div className="contact-info-section">
       <h1>Let´s talk</h1>
       <img
         src={profilePicture}
@@ -15,6 +21,23 @@ export const ContactInfo = () => {
         <p>+46(0)76 242 42 42</p>
         <p>erikaellerekki@gmail.com</p>
       </div>
+      <div className="social-media-box">
+        <a href="">
+          <img src={linkedIn} alt="LinkedIn-icon" className="icon" />
+        </a>
+        <a href="">
+          <img src={githubSmall} alt="Github-icon" className="icon" />
+        </a>
+        <a href="">
+          <img src={stackOverflow} alt="StackOverflow-icon" className="icon" />
+        </a>
+        <a href="">
+          <img src={blueSky} alt="Bluesky-icon" className="icon" />
+        </a>
+        <a href="">
+          <img src={instagram} alt="Instagram-icon" className="icon" />
+        </a>
+      </div>
     </div>
   );
-};
+}
